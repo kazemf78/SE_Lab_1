ea8fbad39a76f08014f1a4c83a5f346709122f0b
Kazem Faghih <kazemf78@yahoo.com>
2021-07-23 14:47:01 +0430

    define todo-task model

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Task %r>" % self.id
 
 @app.route("/")
 def index():
